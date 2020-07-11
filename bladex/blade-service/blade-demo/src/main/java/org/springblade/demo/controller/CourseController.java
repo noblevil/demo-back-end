071ed3bbf15fdf6ea00a1293ea491bf34dbfaac8
cyf <1732562137@qq.com>
2020-07-12 00:07:59 +0800

    新增根据机构账户查询教师和课程信息四个api

--- bladex/blade-service/blade-demo/src/main/java/org/springblade/demo/controller/CourseController.java
+++ bladex/blade-service/blade-demo/src/main/java/org/springblade/demo/controller/CourseController.java
@@ -21,15 +21,26 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport;
 import lombok.AllArgsConstructor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.aspectj.weaver.patterns.IToken;
 import org.springblade.core.mp.support.Condition;
 import org.springblade.core.mp.support.Query;
 import org.springblade.core.tool.api.R;
 import org.springblade.core.tool.utils.Func;
 import org.springblade.demo.annotation.JwtIgnore;
+import org.springblade.demo.annotation.Role;
+import org.springblade.demo.common.RoleCode;
 import org.springblade.demo.entity.CourseClass;
+import org.springblade.demo.entity.OrgAccount;
+import org.springblade.demo.entity.OrgInfo;
 import org.springblade.demo.service.ICourseClassService;
+import org.springblade.demo.service.IOrgAccountService;
+import org.springblade.demo.service.IOrgInfoService;
+import org.springblade.demo.util.JwtTokenUtil;
 import org.springframework.web.bind.annotation.*;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import org.springblade.demo.entity.Course;
@@ -39,6 +50,7 @@ import org.springblade.core.boot.ctrl.BladeController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * 课程内容  控制器
@@ -54,6 +66,8 @@ public class CourseController extends BladeController {
 
 	private ICourseService courseService;
 	private ICourseClassService courseClassService;
+	private IOrgInfoService orgInfoService;
+	private IOrgAccountService orgAccountService;
 
 
 	/**
@@ -139,6 +153,117 @@ public class CourseController extends BladeController {
 		List<CourseClass> list = courseClassService.list(Condition.getQueryWrapper(courseClass));
 		return R.data(list);
 	}
+
+	/**
+	 * 筛选：根据机构名获取机构所属的课程（返回课程信息）——7.11
+	 * http://cyf.ngrok2.xiaomiqiu.cn/course/queryCourse?condition=机构名或机构id
+	 */
+	@JwtIgnore
+//	@Role(include = {RoleCode.TEACH})
+	@GetMapping("/queryCourse")
+	@ApiOperationSupport(order = 1)
+	@ApiOperation(value = "条件查询：获取机构所属的课程名", notes = "传入机构名")
+	public  R<List<Course>> queryCourseByOrgId(String condition) {
+		Course courseCondition=new Course();
+		Pattern pattern = Pattern.compile("[0-9]*");
+		boolean type=pattern.matcher(condition).matches();  //用正则表达式判断传入的参数类型，若为数字，返回true
+		if(type) {  //数字类型
+			Integer orgId = Integer.valueOf(condition);
+			System.out.println("传入机构id=" + orgId);
+			courseCondition.setOrgId(orgId);  //设置筛选条件
+		}
+		else {
+			//根据机构名获取机构id
+			OrgInfo orgCondition=new OrgInfo();  //筛选条件
+			orgCondition.setOrgName(condition);
+			OrgInfo orgInfo=orgInfoService.getOne(Condition.getQueryWrapper(orgCondition));
+			try{
+				Integer orgId=orgInfo.getOrgId();
+				courseCondition.setOrgId(orgId);
+			}
+			catch (Exception e){
+				return R.fail("机构账户不存在");
+//				return R.data(200,null,"机构账户不存在");
+			}
+		}
+
+		//获取机构所属的所有课程信息
+		List<Course> list = courseService.list(Condition.getQueryWrapper(courseCondition));
+		return R.data(list);
+	}
+
+
+	/**
+	 * 筛选：根据机构名或机构id获取机构所属的课程名称（只返回课程名）——7.11
+	 * http://cyf.ngrok2.xiaomiqiu.cn/course/queryCourseName?condition=机构名或机构id
+	 */
+	@JwtIgnore
+//	@Role(include = {RoleCode.TEACH})
+	@GetMapping("/queryCourseName")
+	@ApiOperationSupport(order = 1)
+	@ApiOperation(value = "条件查询：获取机构所属的课程名", notes = "传入机构名或机构id")
+	public  R<List<String>> queryCourseName(String condition) {
+
+		Course courseCondition=new Course();
+		Pattern pattern = Pattern.compile("[0-9]*");
+		boolean type=pattern.matcher(condition).matches();  //用正则表达式判断传入的参数类型，若为数字，返回true
+		if(type) {  //数字类型
+			Integer orgId = Integer.valueOf(condition);
+			System.out.println("传入机构id=" + orgId);
+			courseCondition.setOrgId(orgId);  //设置筛选条件
+		}
+		else {
+			//根据机构名获取机构id
+			OrgInfo orgCondition=new OrgInfo();  //筛选条件
+			orgCondition.setOrgName(condition);
+			OrgInfo orgInfo=orgInfoService.getOne(Condition.getQueryWrapper(orgCondition));
+			try{
+				Integer orgId=orgInfo.getOrgId();
+				courseCondition.setOrgId(orgId);
+			}
+			catch (Exception e){
+				return R.fail("机构账户不存在");
+			}
+		}
+
+		//获取机构所属的所有课程信息
+		List<Course> list = courseService.list(Condition.getQueryWrapper(courseCondition));
+		//提取课程名字段
+		List<String> courseNames = new ArrayList<>();
+		for(int i=0;i<list.size();++i){
+			String name=list.get(i).getCourseName();
+			courseNames.add(name);
+		}
+		return R.data(courseNames);
+	}
+
+	/**
+	 * 筛选：根据机构账户名获取机构所属的课程——7.11
+	 * 例子：http://localhost:9101/course/getOrgCourseByOrgAccount?orgAccount=1101234561
+	 * http://cyf.ngrok2.xiaomiqiu.cn/course/getOrgCourseByOrgAccount?orgAccount=机构账户名
+	 */
+	@JwtIgnore
+//	@Role(include = {RoleCode.ORG})
+	@GetMapping("/getOrgCourseByOrgAccount")
+	@ApiOperationSupport(order = 1)
+	@ApiOperation(value = "条件查询：根据机构账户名获取机构所属的课程", notes = "传入机构账户名")
+	public  R<List<Course>> getOrgCourseByOrgAccount(String orgAccount) {
+		OrgAccount orgAccountCondition=new OrgAccount();
+		orgAccountCondition.setOrgAccount(orgAccount);
+		//根据机构账户名获取机构id
+		try{
+			int orgId=orgAccountService.getOne(Condition.getQueryWrapper(orgAccountCondition)).getOrgId();
+			//根据机构名获取机构id
+			Course courseCondition=new Course();
+			courseCondition.setOrgId(orgId);
+			List<Course> list = courseService.list(Condition.getQueryWrapper(courseCondition));
+
+			return R.data(list);
+		}catch (Exception e){
+			return R.fail("机构账户不存在");
+		}
+	}
+
 
 	//===========================以下为自动生成的接口==============================
 
--- bladex/blade-service/blade-demo/src/main/java/org/springblade/demo/controller/TeachInfoController.java
+++ bladex/blade-service/blade-demo/src/main/java/org/springblade/demo/controller/TeachInfoController.java
@@ -16,6 +16,7 @@
  */
 package org.springblade.demo.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -28,14 +29,21 @@ import org.springblade.core.mp.support.Query;
 import org.springblade.core.tool.api.R;
 import org.springblade.core.tool.utils.Func;
 import org.springblade.demo.annotation.JwtIgnore;
+import org.springblade.demo.annotation.Role;
+import org.springblade.demo.common.RoleCode;
+import org.springblade.demo.entity.OrgAccount;
 import org.springblade.demo.entity.RelOrgTeach;
+import org.springblade.demo.entity.TeachAccount;
+import org.springblade.demo.service.IOrgAccountService;
 import org.springblade.demo.service.IRelOrgTeachService;
+import org.springblade.demo.service.ITeachAccountService;
 import org.springframework.web.bind.annotation.*;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import org.springblade.demo.entity.TeachInfo;
 import org.springblade.demo.vo.TeachInfoVO;
 import org.springblade.demo.service.ITeachInfoService;
 import org.springblade.core.boot.ctrl.BladeController;
+import sun.text.resources.no.CollationData_no;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -54,6 +62,8 @@ public class TeachInfoController extends BladeController {
 
 	private ITeachInfoService teachInfoService;
 	private IRelOrgTeachService relOrgTeachService;
+	private ITeachAccountService teachAccountService;
+	private IOrgAccountService orgAccountService;
 
 	/**
 	 * 根据机构id获取所属的教师列表
@@ -124,6 +134,76 @@ public class TeachInfoController extends BladeController {
 			if(teachDetail!=null) resultList.add(teachDetail);
 		}
 		return R.data(resultList);
+	}
+
+	/**
+	 * 查询：根据teachAccount获取教师基本信息——7.11
+	 * http://localhost:9101/teachinfo/getProfile?teachAccount=教师账户名
+	 * http://cyf.ngrok2.xiaomiqiu.cn/teachinfo/getProfile?teachAccount=教师账户名
+	 * http://cyf.ngrok2.xiaomiqiu.cn/teachinfo/getProfile?teachAccount=110
+	 */
+	@JwtIgnore
+//	@Role(include = {RoleCode.TEACH})
+	@GetMapping("/getProfile")
+	@ApiOperationSupport(order = 1)
+	@ApiOperation(value = "条件查询：获取教师信息详情", notes = "传入教师账户名")
+	public R<JSONObject> getProfile(String teachAccount) {
+		TeachAccount teachAccountCondition=new TeachAccount();
+		teachAccountCondition.setTeachAccount(teachAccount);
+		//根据teachAccount查询教师id
+		try{
+			int teachId=teachAccountService.getOne(Condition.getQueryWrapper(teachAccountCondition)).getTeachId();
+			//设置查询条件
+			TeachInfo teachInfoCondition=new TeachInfo();
+			teachInfoCondition.setTeachId(teachId);
+			RelOrgTeach relOrgTeachCondition=new RelOrgTeach();
+			relOrgTeachCondition.setTeachId(teachId);
+			//结果返回
+			JSONObject result=new JSONObject();
+			result.put("teachInfo",teachInfoService.getOne(Condition.getQueryWrapper(teachInfoCondition)));
+			result.put("teachAccount",teachAccountService.getOne(Condition.getQueryWrapper(teachAccountCondition)));
+			result.put("relOrgTeach",relOrgTeachService.list(Condition.getQueryWrapper(relOrgTeachCondition)));
+			return R.data(result);
+		}
+		catch (Exception e){
+			return R.fail("教师账户不存在");
+		}
+	}
+
+	/**
+	 * 根据机构账户获取机构所属教师信息——7.11
+	 * 例如：http://localhost:9101/teachinfo/getOrgTeacherByOrgAccount?orgAccount=1101234561
+	 * http://cyf.ngrok2.xiaomiqiu.cn/teachinfo/getOrgTeacherByOrgAccount?orgAccount=机构账户名
+	 */
+	@JwtIgnore
+//	@Role(include = {RoleCode.ORG})
+	@GetMapping("/getOrgTeacherByOrgAccount")
+	@ApiOperationSupport(order = 1)
+	@ApiOperation(value = "根据机构账户获取机构所属教师信息", notes = "传入机构账户名")
+	public  R<List<TeachInfo>> getOrgTeacherByOrgAccount(String orgAccount) {
+		OrgAccount orgAccountCondition=new OrgAccount();
+		orgAccountCondition.setOrgAccount(orgAccount);
+		//根据机构账户名获得机构id
+		try{
+			int orgId=orgAccountService.getOne(Condition.getQueryWrapper(orgAccountCondition)).getOrgId();
+			//获取机构下所有教师的id
+			RelOrgTeach relOrgTeachCondition=new RelOrgTeach();
+			relOrgTeachCondition.setOrgId(orgId);
+			List<RelOrgTeach> teachIds=relOrgTeachService.list(Condition.getQueryWrapper(relOrgTeachCondition));
+			//根据教师id获取教师信息
+			List<TeachInfo> result=new ArrayList<>();
+			for(int i=0;i<teachIds.size();++i)
+			{
+				int teachId=teachIds.get(i).getTeachId();
+				TeachInfo teachInfoCondition=new TeachInfo();
+				teachInfoCondition.setTeachId(teachId);
+				TeachInfo info=teachInfoService.getOne(Condition.getQueryWrapper(teachInfoCondition));
+				result.add(info);
+			}
+			return R.data(result);
+		}catch (Exception e){
+			return R.fail("机构账户不存在");
+		}
 	}
 
 	//===========================以下为自动生成的接口==============================
